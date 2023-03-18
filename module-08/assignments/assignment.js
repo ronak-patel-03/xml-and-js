f8abac810818b6c35fed5a73eefd8723a5c95b24
Ronak Patel <ronakpatel030900@gmail.com>
2023-03-18 17:08:47 -0400

    module8 assignment

--- module-08/assignments/assignment.js
+++ module-08/assignments/assignment.js
@@ -1,24 +1,26 @@
-const timeout = async (ms) => {return setTimeout(() => ms);};
+const timeout = async(ms) => {
+	return setTimeout(() => ms);
+};
 
 const generateRandomNumber = (()=>Math.floor(Math.random() * 40));
 
-const generateData = async () => {
+const generateData = async() => {
 	await timeout(1000);
 	return processData(Array.from({ length: 20 }, generateRandomNumber));
 };
 
-const convertToFeet = async (meters) => {
+const convertToFeet = async(meters) => {
 	await timeout(3500);
 	return logResult(meters, meters * 3.2808);
 };
 
-const processData = async (data) => {
+const processData = async(data) => {
     data.map(async (value) => {
     return convertToFeet(value);
     });
 }
 
-const logResult = async (meters, feet) => {
+const logResult = async(meters, feet) => {
     console.log(`Converted ${meters}m to ${feet}ft`);
 }
 
@@ -27,5 +29,4 @@ async function main() {
     await generateData();
     console.log("Finish");
 }
-
-main();
+main()
